f2d2eea0db6b1c0765fca1cc3bd7b7cd7ce70e82
Markeisha <iammarkeisha@gmai.com>
2023-03-01 14:10:51 -0600

    Added styling to SideNav.css

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -10,18 +10,39 @@ export default function SideNav() {
             </div>
             <div className='nav-links2'>
                 <a href='' className='menu2'>MENU</a>
-                    <br/>
+                    <br/><br/>
+
+                    <div class="menu-line1"></div>
+
                 <a href='' className='join2'>JOIN <br /> THE WAIT</a>
-                    <br/>
-                <a href='' className='rewards2'>OURSTREET <br /> REWARDS</a>
-                    <br/>
+                    <br/><br/>
+
+                    <div class="menu-line2"></div>
+
+                <a href='' className='rewards2'>OURSTREET <br /> REWARD$</a>
+                    <br/><br/>
+
+                    <div class="menu-line3"></div>
+
                 <a href='' className='giftcards2'>GIFT<br />CARDS</a>
-                    <br/>
+                    <br/><br/>
+
+                    <div class="menu-line4"></div>
+
                 <a href='' className='locations2'>LOCATIONS</a>
-                    <br/>
+                    <br/><br/>
+
+                    <div class="menu-line5"></div>
+
                 <a href='' className='contact2'>CONTACT</a>
-                    <br/>
+                    <br/><br/>
+
+                    <div class="menu-line6"></div>
+
                 <a href='' className='careers2'>CAREERS</a>
+                    <br/><br/>
+
+                    <div class="menu-line7"></div>
             </div>
             
         </div>
